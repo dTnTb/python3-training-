71e240f0dd1d447c0d7c141db389f95a39259bea
dTnTb <1053684859@qq.com>
2018-11-18 10:00:46 -0800

    decorator

--- function/decorator.py
+++ function/decorator.py
@@ -1,4 +1,4 @@
-def log(text):
+def log(text = ''): #fault arg 
     print('1')
     def decorator(func):
         print('2')
@@ -16,8 +16,15 @@ def log(text):
 
 @log('execute')
 def now():
+    print('execute now is 11/18')
+    return 
+
+@log('')
+def now2():
     print('now is 11/18')
     return 
 
 f1 = now
 f1()
+f2 = now2
+f2()
